82f3dca08bf12b6d36ee5aef0e3d44cdc3f4f268
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-03-08 12:53:39 +0100

    feat: update useDebounce demo

--- content/hooks/use-debounce.tsx
+++ content/hooks/use-debounce.tsx
@@ -3,25 +3,51 @@
 import * as React from "react";
 import { Loader2Icon, SearchIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useDebounce } from "@/hooks/use-debounce";
 
 export default function UseDebounceDemo() {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const [value, setValue] = React.useState<string>("");
+  const [searchInput, setSearchInput] = React.useState<string>("");
+  const debouncedValue = useDebounce<string>(searchInput, 1000);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsLoading(false)
+    setSearchInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+    // fetch here
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  React.useEffect(() => {
+    handleSubmit();
+  }, [debouncedValue]);
 
   return (
     <div className="relative w-52">
       {isLoading ? (
-        <Loader2Icon
-          size={18}
-          className="pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 transform animate-spin text-muted-foreground"
-        />
+        <div className="absolute left-2 h-full flex items-center">
+          <Loader2Icon
+            size={18}
+            className="pointer-events-none animate-spin text-muted-foreground"
+          />
+        </div>
       ) : (
         <SearchIcon
           size={18}
           className="pointer-events-none absolute left-2 top-1/2 -translate-y-1/2 transform text-muted-foreground"
         />
       )}
-      <Input placeholder="Search items" className="full-w pl-8" />
+      <Input
+        value={searchInput}
+        onChange={handleChange}
+        placeholder="Search items"
+        className="full-w pl-8"
+      />
     </div>
   );
 }
