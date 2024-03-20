9a1a10f9d55fd3ed07afad4045e3a6565572fd52
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-03-20 01:57:47 +0100

    fix: fix copy button icon fade-in

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { LayoutGroup, motion } from "framer-motion";
 import { CheckIcon, CopyIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/utils/classes";
@@ -22,31 +21,11 @@ export const CopyButton = ({ code, className }: { code: string; className?: stri
       onClick={handleCopy}
       className={cn("h-8 w-8", className)}
     >
-      <LayoutGroup>
-        {copied ? (
-          <motion.span
-            layoutId="icon"
-            key={0}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            <CheckIcon size={15} />
-          </motion.span>
-        ) : (
-          <motion.span
-            layoutId="icon"
-            key={1}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            <CopyIcon size={15} />
-          </motion.span>
-        )}
-      </LayoutGroup>
+      {copied ? (
+        <CheckIcon size={15} className="animate-in fade-in" />
+      ) : (
+        <CopyIcon size={15} className="animate-in fade-in" />
+      )}
     </Button>
   );
 };
