749f7a2cb9e7bb3be0e0f46f720027687889c2de
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-04-05 13:35:54 +0100

    fix: search command minor fixes

--- src/components/docs/search-docs.tsx
+++ src/components/docs/search-docs.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 import { CircleIcon, SearchIcon } from "lucide-react";
 import { Button, type ButtonProps } from "@/lib/components/core/default/button";
 import {
@@ -16,6 +17,33 @@ import { docsConfig } from "@/config/docs-config";
 
 export const SearchDocs = (props: ButtonProps) => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if ((e.key === "k" && (e.metaKey || e.ctrlKey)) || e.key === "/") {
+        if (
+          (e.target instanceof HTMLElement && e.target.isContentEditable) ||
+          e.target instanceof HTMLInputElement ||
+          e.target instanceof HTMLTextAreaElement ||
+          e.target instanceof HTMLSelectElement
+        ) {
+          return;
+        }
+
+        e.preventDefault();
+        setIsOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const runCommand = React.useCallback((command: () => unknown) => {
+    setIsOpen(false);
+    command();
+  }, []);
 
   return (
     <>
@@ -38,7 +66,13 @@ export const SearchDocs = (props: ButtonProps) => {
               {category.items.map((item, itemIndex) => {
                 if ("href" in item && item.href) {
                   return (
-                    <CommandItem key={itemIndex} className="flex items-center space-x-2">
+                    <CommandItem
+                      key={itemIndex}
+                      onSelect={() => {
+                        runCommand(() => router.push(item.href));
+                      }}
+                      className="flex items-center space-x-2"
+                    >
                       <CircleIcon />
                       <span>{item.title}</span>
                     </CommandItem>
@@ -51,6 +85,9 @@ export const SearchDocs = (props: ButtonProps) => {
                         return (
                           <CommandItem
                             key={subItemIndex}
+                            onSelect={() => {
+                              runCommand(() => router.push(subItem.href));
+                            }}
                             className="flex items-center justify-between"
                           >
                             <div className="flex items-center space-x-2">
