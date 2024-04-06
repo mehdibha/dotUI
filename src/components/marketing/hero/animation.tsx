79b168b50833339a11d5ee775c9becf62a721a00
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-04-06 23:26:08 +0100

    fix: fix animation duration

--- src/components/marketing/hero/animation.tsx
+++ src/components/marketing/hero/animation.tsx
@@ -31,9 +31,9 @@ export const Animation = () => {
   }, [animationStarted]);
 
   const animationProps = {
-    initial: { y: 10, opacity: 0 },
+    initial: { y: 12, opacity: 0 },
     animate: { y: 0, opacity: 1 },
-    transition: { duration: 0.4 },
+    transition: { duration: 0.5 },
     layoutId: "underline",
   };
 
