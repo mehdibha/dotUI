f4fb6c0029d423461c758c91dd8b1f0fea996714
yutasb <shibamoto.yuta@gmail.com>
2024-07-25 22:54:49 +0900

    fix: Add missing href to the back home button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="mt-32 flex flex-col items-center space-x-4">
       <h1 className="text-4xl dark:text-white">404</h1>
       <p className="text-lg text-stone-500 dark:text-stone-400">Not found</p>
-      <Button className="mt-4">Back to home</Button>
+      <Button href="/" className="mt-4">Back to home</Button>
     </div>
   );
 }
