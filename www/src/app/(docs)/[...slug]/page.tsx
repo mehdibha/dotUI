0d5be83e7c2d8705c11964e628cb94e213319881
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-10-05 13:54:44 +0100

    chore: migrate to fumadocs

--- www/src/app/(docs)/[...slug]/page.tsx
+++ www/src/app/(docs)/[...slug]/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { ExternalLinkIcon } from "lucide-react";
+import { source } from "@/app/source";
 import { DocsPager } from "@/components/docs-pager";
-import { TableOfContents } from "@/components/toc";
-import { Button } from "@/registry/ui/default/core/button";
-import { cn } from "@/registry/ui/default/lib/cn";
-import { allDocs, getDocBySlug } from "@/lib/docs";
-import { MDXContent } from "@content-collections/mdx/react";
 import { components } from "@/components/mdx-components";
+import { TableOfContents } from "@/components/toc";
 import { siteConfig } from "@/config";
 import { truncateOnWord } from "@/lib/string";
-import { getTableOfContents } from "fumadocs-core/server";
+import { Button } from "@/registry/ui/default/core/button";
+import { cn } from "@/registry/ui/default/lib/cn";
+import { MDXContent } from "@content-collections/mdx/react";
+import { type TableOfContents as TocType } from "fumadocs-core/server";
+import { ExternalLinkIcon } from "lucide-react";
 
 interface PageProps {
   params: {
@@ -21,71 +21,29 @@ interface PageProps {
 
 const config = siteConfig.global;
 
-export function generateMetadata({ params }: PageProps): Metadata {
-  const doc = getDocBySlug(params.slug);
-
-  if (!doc) {
-    return {};
-  }
-
-  const metadata: Metadata = {
-    title: doc.title,
-    description: doc.description,
-    openGraph: {
-      title: doc.title,
-      description: doc.description
-        ? truncateOnWord(doc.description, 148, true)
-        : undefined,
-      type: "article",
-      url: `${config.url}${doc._meta.path}`,
-      images: [config.thumbnail],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: doc.title,
-      description: doc.description
-        ? truncateOnWord(doc.description, 148, true)
-        : undefined,
-      images: [config.thumbnail],
-      creator: config.twitter.creator,
-    },
-  };
-
-  return metadata;
-}
-
-export function generateStaticParams(): PageProps["params"][] {
-  return allDocs.map((doc) => ({ slug: doc._meta.path.split("/") }));
-}
-
 export default async function Page({ params }: PageProps) {
-  const doc = getDocBySlug(params.slug);
-
-  if (!doc) {
-    notFound();
-  }
-
-  const toc = await getTableOfContents(doc.rawContent);
-  const hasToc = toc.length > 0;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
 
   return (
     <div
       className={cn("relative py-20", {
-        "lg:grid lg:grid-cols-[1fr_230px] lg:gap-10": hasToc,
+        "lg:grid lg:grid-cols-[1fr_230px] lg:gap-10":
+          page.data.toc && page.data.toc.length > 0,
       })}
     >
       <div className="container max-w-3xl">
-        <h1 className="text-4xl font-bold">{doc.title}</h1>
-        <p className="mt-2 text-fg-muted">{doc.description}</p>
-        {doc.links && doc.links.length > 0 && (
+        <h1 className="text-4xl font-bold">{page.data.title}</h1>
+        <p className="text-fg-muted mt-2">{page.data.description}</p>
+        {page.data.links && page.data.links.length > 0 && (
           <div className="mt-4 flex flex-wrap gap-2">
-            {doc.links.map((link, index) => (
+            {page.data.links.map((link, index) => (
               <Button
                 key={index}
                 href={link.href}
                 suffix={<ExternalLinkIcon />}
                 size="sm"
-                className="[&_svg]:size-3 h-6 text-xs font-semibold"
+                className="h-6 text-xs font-semibold [&_svg]:size-3"
                 target="_blank"
               >
                 {link.label}
@@ -94,19 +52,51 @@ export default async function Page({ params }: PageProps) {
           </div>
         )}
         <div className="mt-10 text-sm md:text-base">
-          <MDXContent code={doc.content} components={components} />
+          <MDXContent code={page.data.body} components={components} />
         </div>
         <div className="mt-20">
-          <DocsPager currentPagePath={`/${doc._meta.path}`} />
+          <DocsPager currentPagePath={`/${page.data._meta.path}`} />
         </div>
       </div>
-      {hasToc && (
+      {page.data.toc && page.data.toc.length > 0 && (
         <div className="hidden text-sm lg:block">
           <div className="sticky top-0 pt-8">
-            <TableOfContents toc={toc} />
+            <TableOfContents toc={page.data.toc as TocType} />
           </div>
         </div>
       )}
     </div>
   );
+}
+
+export function generateMetadata({ params }: PageProps): Metadata {
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  return {
+    title: page.data.title,
+    description: page.data.description,
+    openGraph: {
+      title: page.data.title,
+      description: page.data.description
+        ? truncateOnWord(page.data.description, 148, true)
+        : undefined,
+      type: "article",
+      url: page.url,
+      images: [config.thumbnail],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: page.data.title,
+      description: page.data.description
+        ? truncateOnWord(page.data.description, 148, true)
+        : undefined,
+      images: [config.thumbnail],
+      creator: config.twitter.creator,
+    },
+  };
+}
+
+export function generateStaticParams(): PageProps["params"][] {
+  return source.generateParams();
 }
