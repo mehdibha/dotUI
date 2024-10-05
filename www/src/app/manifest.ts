e05a7456456d509ec7a583ecc16298e2fb9b289c
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-10-05 04:18:01 +0100

    chore: add manifest.ts

---
+++ www/src/app/manifest.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+import { siteConfig } from "@/config/site-config";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "dotUI",
+    short_name: "dotui",
+    description: siteConfig.global.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#010101",
+    theme_color: "#010101",
+    icons: [
+      {
+        src: "https://dotui.org/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
