84d64bceef63d4ae9434a8d038821f592c358903
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-10-11 16:14:30 +0100

    fix: minor fixes

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,22 +1,12 @@
-"use client";
-
 import React from "react";
 import { TerminalSquareIcon } from "lucide-react";
-import { useCommandMenuInputRef } from "@/hooks/use-focus-command-menu";
-// import { Code } from "@/components/code";
+import { Code } from "@/components/code";
 import { SearchCommand } from "@/components/search-command";
 import { Link } from "@/registry/ui/default/core/link";
 import { cn } from "@/registry/ui/default/lib/cn";
 import { siteConfig } from "@/config";
 
 export default function HomePage() {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const { setInputRef } = useCommandMenuInputRef();
-
-  React.useEffect(() => {
-    setInputRef(inputRef);
-  }, [setInputRef]);
-
   return (
     <div className="absolute inset-0 h-full overflow-hidden">
       <div className="relative size-full">
@@ -34,7 +24,7 @@ export default function HomePage() {
                 <div className="z-[-1] h-[450px] w-[700px] bg-[radial-gradient(at_0%_0%,#0894ff_0,rgba(0,0,0,0)_40%),radial-gradient(at_50%_30%,#ff2e54_0,rgba(0,0,0,0)_60%),radial-gradient(at_100%_0%,#ff9004_0,rgba(0,0,0,0)_40%)] blur-[100px]" />
               </div>
               <SearchCommand
-                inputRef={inputRef}
+                context
                 animated
                 className="mt-6 h-64 w-full lg:min-w-[600px]"
               />
@@ -42,7 +32,7 @@ export default function HomePage() {
           </div>
           {/* footer */}
           <div className="absolute bottom-5 left-0 right-0 z-0 flex flex-col items-center justify-center gap-6">
-            {/* <Terminal>{`npx dotui@latest init`}</Terminal> */}
+            <Terminal>{`npx dotui@latest init`}</Terminal>
             <p className="text-fg-muted px-4 text-xs">
               Built by{" "}
               <Link
@@ -68,27 +58,27 @@ export default function HomePage() {
   );
 }
 
-// const Terminal = ({
-//   children,
-//   className,
-// }: {
-//   children: React.ReactNode;
-//   className?: string;
-// }) => {
-//   return (
-//     <div className={cn("w-56 rounded-md border", className)}>
-//       <div className="bg-bg-muted rounded-t-[inherit] border-b p-1">
-//         <TerminalSquareIcon className="text-fg-muted size-3" />
-//       </div>
-//       <Code
-//         lang="bash"
-//         colorReplacements={{
-//           "#96d0ff": "#ffffff",
-//         }}
-//         className="rounded-t-none bg-black/20"
-//       >
-//         {children}
-//       </Code>
-//     </div>
-//   );
-// };
+const Terminal = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("w-56 rounded-md border", className)}>
+      <div className="bg-bg-muted rounded-t-[inherit] border-b p-1">
+        <TerminalSquareIcon className="text-fg-muted size-3" />
+      </div>
+      <Code
+        lang="bash"
+        colorReplacements={{
+          "#96d0ff": "#ffffff",
+        }}
+        className="rounded-t-none bg-black/20"
+      >
+        {children}
+      </Code>
+    </div>
+  );
+};
