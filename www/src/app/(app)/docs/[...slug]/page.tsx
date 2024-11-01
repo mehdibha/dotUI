1aa87cdb39b0afc364529b77beb13710769a4146
Mehdi BHA <mehdibenhadjali@gmail.com>
2024-11-01 01:09:05 +0100

    chore: run codemod

--- www/src/app/(app)/docs/[...slug]/page.tsx
+++ www/src/app/(app)/docs/[...slug]/page.tsx
@@ -17,14 +17,15 @@ import { source } from "@/app/source";
 import { siteConfig } from "@/config";
 
 interface PageProps {
-  params: {
+  params: Promise<{
     slug: string[];
-  };
+  }>;
 }
 
 const config = siteConfig.global;
 
-export default async function Page({ params }: PageProps) {
+export default async function Page(props: PageProps) {
+  const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
   const MDXContent = page.data.body;
@@ -82,7 +83,8 @@ const getIcon = (url: string) => {
   return null;
 };
 
-export function generateMetadata({ params }: PageProps): Metadata {
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+  const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
 
