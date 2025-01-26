e299b94ae1fc330e65e82256184b1a48102bf931
Mehdi BHA <mehdibenhadjali@gmail.com>
2025-01-26 23:48:39 +0100

    chore: update separator

--- www/src/registry/core/separator.tsx
+++ www/src/registry/core/separator.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import * as React from "react";
-import {
-  Separator as AriaSeparator,
-  type SeparatorProps as AriaSeparatorProps,
-} from "react-aria-components";
+import React from "react";
+import { Separator as AriaSeparator } from "react-aria-components";
 import { tv } from "tailwind-variants";
 
 const separatorStyles = tv({
   base: "bg-border separator shrink-0 border-0",
   variants: {
     orientation: {
-      horizontal: "h-[1px] w-full",
-      vertical: "h-full w-[1px]",
+      horizontal: "h-px w-full",
+      vertical: "h-full w-px",
     },
   },
   defaultVariants: {
@@ -20,12 +17,12 @@ const separatorStyles = tv({
   },
 });
 
-type SeparatorProps = AriaSeparatorProps;
+interface SeparatorProps extends React.ComponentProps<typeof AriaSeparator> {}
 const Separator = ({ orientation, className, ...props }: SeparatorProps) => {
   return (
     <AriaSeparator
-      {...props}
       className={separatorStyles({ orientation, className })}
+      {...props}
     />
   );
 };
