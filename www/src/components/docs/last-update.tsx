ca1e4570a161ff61619dfb3e4fd95fe79c900c06
Mehdi BHA <mehdibenhadjali@gmail.com>
2025-02-08 16:57:56 +0100

    fix: update github last edit

--- www/src/components/docs/last-update.tsx
+++ www/src/components/docs/last-update.tsx
@@ -1,12 +1,16 @@
 import { getGithubLastEdit } from "@/lib/docs/get-github-last-edit";
 
 export async function PageLastUpdate({ path }: { path: string }) {
-  const date = await getGithubLastEdit(path);
-
-  if (!date) return null;
-  return (
-    <p className="text-fg-muted text-sm">
-      Last updated on {date.toLocaleDateString()}
-    </p>
-  );
+  try {
+    const date = await getGithubLastEdit(path);
+    if (!date) return null;
+    return (
+      <p className="text-fg-muted text-sm">
+        Last updated on {date.toLocaleDateString()}
+      </p>
+    );
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 }
