d239f72fb34d2031517ef18a7ef3da362e87530e
Mehdi BHA <mehdibenhadjali@gmail.com>
2025-04-10 19:20:51 +0100

    update next config

---
+++ www/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const config: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
+
+export default config;
