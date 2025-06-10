cc4d3dfd98f0d11a68ed138971c48d8beb51bcac
mehdibha <hello@mehdibha.com>
2025-06-10 17:30:27 +0100

    update eslint

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,7 @@
-import { neon } from "@neondatabase/serverless";
+import {  } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+
 export const db = drizzle({ client: sql, schema, casing: "snake_case" });
