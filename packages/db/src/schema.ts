1ce136760f16aa347489585a706a0137b8eed87b
mehdibha <hello@mehdibha.com>
2025-06-27 11:27:12 +0100

    update schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,1 +1,17 @@
+import { sql } from "drizzle-orm";
+import { pgTable } from "drizzle-orm/pg-core";
+
+import type { StyleDefinition } from "@dotui/style-engine/types";
+
+export const Style = pgTable("style", (t) => ({
+  id: t.uuid().notNull().primaryKey().defaultRandom(),
+  name: t.varchar({ length: 256 }).notNull(),
+  description: t.text(),
+  definition: t.jsonb("definition").$type<StyleDefinition>().notNull(),
+  createdAt: t.timestamp().defaultNow().notNull(),
+  updatedAt: t
+    .timestamp({ mode: "date", withTimezone: true })
+    .$onUpdateFn(() => sql`now()`),
+}));
+
 export * from "./auth-schema";
--- packages/style-engine/src/types.ts
+++ packages/style-engine/src/types.ts
@@ -64,7 +64,7 @@ export interface Variants {
   tooltip: "basic" | "motion";
 }
 
-export interface Style {
+export interface StyleDefinition {
   name: string;
   label?: string;
   description?: string;
