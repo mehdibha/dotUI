3fb764dec6442a86382d27f9a0b9c2eedb816ded
mehdibha <hello@mehdibha.com>
2025-08-07 23:21:33 +0100

    format

--- packages/db/src/schemas/auth.ts
+++ packages/db/src/schemas/auth.ts
@@ -1,6 +1,6 @@
-import type { AnyPgColumn } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { pgTable } from "drizzle-orm/pg-core";
+import type { AnyPgColumn } from "drizzle-orm/pg-core";
 
 import { style } from "./style";
 
