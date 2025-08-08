52e744176ff6cb30293a36ef3e2566732916e706
mehdibha <hello@mehdibha.com>
2025-08-08 13:49:22 +0100

    fix url

--- www/app/(app)/styles/(style)/[username]/[styleName]/page.tsx
+++ www/app/(app)/styles/(style)/[username]/[styleName]/page.tsx
@@ -1,6 +1,6 @@
 import { StyleColorsEditor } from "@/modules/styles/components/style-colors-editor";
 
-export const dynamic = "error";
+export const dynamic = "force-static";
 
 export default function StyleColorsPage() {
   return <StyleColorsEditor />;
