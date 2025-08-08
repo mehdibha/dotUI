2fd61b4d847bdd8b82508aa67e39607b34577f65
mehdibha <hello@mehdibha.com>
2025-08-08 04:14:49 +0100

    remove middleware

--- www/app/(app)/styles/(style)/[username]/[styleName]/page.tsx
+++ www/app/(app)/styles/(style)/[username]/[styleName]/page.tsx
@@ -1,6 +1,6 @@
 import { StyleColorsEditor } from "@/modules/styles/components/style-colors-editor";
 
-export const dynamic = "force-static";
+export const dynamic = "error";
 
 export default function StyleColorsPage() {
   return <StyleColorsEditor />;
