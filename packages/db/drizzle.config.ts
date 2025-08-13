5762cb137e0165b20ef3a8e1561434b70a7ff380
mehdibha <hello@mehdibha.com>
2025-08-13 13:32:45 +0100

    update drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -4,11 +4,9 @@ if (!process.env.POSTGRES_URL) {
   throw new Error("Missing POSTGRES_URL");
 }
 
-const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
-
 export default {
-  schema: "./src/schema.ts",
+  schema: "./src/schemas/index.ts",
   dialect: "postgresql",
-  dbCredentials: { url: nonPoolingUrl },
+  dbCredentials: { url: process.env.POSTGRES_URL },
   casing: "snake_case",
 } satisfies Config;
