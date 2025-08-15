65681717be2d73ec8668e69652df606013f82cfb
mehdibha <hello@mehdibha.com>
2025-08-15 02:38:04 +0100

    feat: update login page on mobile

--- www/app/(app)/(auth)/login/page.tsx
+++ www/app/(app)/(auth)/login/page.tsx
@@ -14,7 +14,7 @@ export default async function Page() {
   }
 
   return (
-    <div className="relative flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="relative flex min-h-svh w-full">
       <div className="absolute left-4 top-4">
         <Button variant="quiet" prefix={<ArrowLeftIcon />} size="sm" href="/">
           Back to home
