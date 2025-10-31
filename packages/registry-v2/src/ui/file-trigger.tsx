05bdcb93a02b0cdb03b2f44b95edb3557b717ce7
mehdibha <hello@mehdibha.com>
2025-10-31 11:10:42 +0100

    remove registry v2 pkg

--- packages/registry-v2/src/ui/file-trigger.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { FileTrigger } from "react-aria-components";
-import type { FileTriggerProps } from "react-aria-components";
-
-export type { FileTriggerProps };
-export { FileTrigger };
