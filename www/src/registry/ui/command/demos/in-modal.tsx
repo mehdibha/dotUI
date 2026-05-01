83dcee3bdef846a28a011683e51551b9263afad4
mehdibha <hello@mehdibha.com>
2026-05-01 18:33:27 +0100

    wip

--- www/src/registry/ui/command/demos/in-modal.tsx
+++ www/src/registry/ui/command/demos/in-modal.tsx
@@ -27,60 +27,62 @@ export default function Demo() {
 			<Button>Open Command</Button>
 			<Modal>
 				<DialogContent>
-					<Command aria-label="Command menu">
-						<SearchField aria-label="Search" autoFocus>
-							<InputGroup size="lg">
-								<InputGroupAddon>
-									<SearchIcon />
-								</InputGroupAddon>
-								<Input placeholder="Type a command or search..." />
-								<InputGroupAddon>
-									<Button slot="close" className="px-1">
-										<Kbd className="bg-transparent">Esc</Kbd>
-									</Button>
-									<Button variant="quiet" isIconOnly>
-										<XIcon aria-hidden="true" />
-									</Button>
-								</InputGroupAddon>
-							</InputGroup>
-						</SearchField>
-						<ListBox aria-label="Commands" onAction={() => console.log("action")}>
-							<ListBoxSection>
-								<ListBoxSectionHeader>Suggestions</ListBoxSectionHeader>
-								<ListBoxItem textValue="Calendar">
-									<CalendarIcon />
-									<span>Calendar</span>
-								</ListBoxItem>
-								<ListBoxItem textValue="Search Emoji">
-									<SmileIcon />
-									<span>Search Emoji</span>
-								</ListBoxItem>
-								<ListBoxItem textValue="Calculator">
-									<CalculatorIcon />
-									<span>Calculator</span>
-								</ListBoxItem>
-							</ListBoxSection>
-							<Separator />
-							<ListBoxSection>
-								<ListBoxSectionHeader>Settings</ListBoxSectionHeader>
-								<ListBoxItem textValue="Profile">
-									<UserIcon />
-									<span>Profile</span>
-									<Kbd>⌘P</Kbd>
-								</ListBoxItem>
-								<ListBoxItem textValue="Billing">
-									<CreditCardIcon />
-									<span>Billing</span>
-									<Kbd>⌘B</Kbd>
-								</ListBoxItem>
-								<ListBoxItem textValue="Settings">
-									<SettingsIcon />
-									<span>Settings</span>
-									<Kbd>⌘S</Kbd>
-								</ListBoxItem>
-							</ListBoxSection>
-						</ListBox>
-					</Command>
+					{({ close }) => (
+						<Command aria-label="Command menu">
+							<SearchField aria-label="Search" autoFocus>
+								<InputGroup size="lg">
+									<InputGroupAddon>
+										<SearchIcon />
+									</InputGroupAddon>
+									<Input placeholder="Type a command or search..." />
+									<InputGroupAddon>
+										<Button slot="close" onPress={close} className="px-1">
+											<Kbd className="bg-transparent">Esc</Kbd>
+										</Button>
+										<Button variant="quiet" isIconOnly>
+											<XIcon aria-hidden="true" />
+										</Button>
+									</InputGroupAddon>
+								</InputGroup>
+							</SearchField>
+							<ListBox aria-label="Commands" onAction={() => console.log("action")}>
+								<ListBoxSection>
+									<ListBoxSectionHeader>Suggestions</ListBoxSectionHeader>
+									<ListBoxItem textValue="Calendar">
+										<CalendarIcon />
+										<span>Calendar</span>
+									</ListBoxItem>
+									<ListBoxItem textValue="Search Emoji">
+										<SmileIcon />
+										<span>Search Emoji</span>
+									</ListBoxItem>
+									<ListBoxItem textValue="Calculator">
+										<CalculatorIcon />
+										<span>Calculator</span>
+									</ListBoxItem>
+								</ListBoxSection>
+								<Separator />
+								<ListBoxSection>
+									<ListBoxSectionHeader>Settings</ListBoxSectionHeader>
+									<ListBoxItem textValue="Profile">
+										<UserIcon />
+										<span>Profile</span>
+										<Kbd>⌘P</Kbd>
+									</ListBoxItem>
+									<ListBoxItem textValue="Billing">
+										<CreditCardIcon />
+										<span>Billing</span>
+										<Kbd>⌘B</Kbd>
+									</ListBoxItem>
+									<ListBoxItem textValue="Settings">
+										<SettingsIcon />
+										<span>Settings</span>
+										<Kbd>⌘S</Kbd>
+									</ListBoxItem>
+								</ListBoxSection>
+							</ListBox>
+						</Command>
+					)}
 				</DialogContent>
 			</Modal>
 		</Dialog>
