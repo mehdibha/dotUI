962e246b2e38b10381fa930f811b0b40343f5304
mehdibha <hello@mehdibha.com>
2026-05-16 16:33:34 +0100

    chore: remove vite tsconfig paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
+
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
+	resolve: {
+		alias: [
+			{ find: /^@\/\.source\//, replacement: `${path.resolve(import.meta.dirname, "www/.source")}/` },
+			{ find: /^@\//, replacement: `${path.resolve(import.meta.dirname, "www/src")}/` },
+		],
+	},
 	test: {
 		exclude: [...configDefaults.exclude, "**/node_modules/**", "**/fixtures/**", "**/templates/**"],
 	},
-	plugins: [
-		tsconfigPaths({
-			ignoreConfigErrors: true,
-		}),
-	],
 });
